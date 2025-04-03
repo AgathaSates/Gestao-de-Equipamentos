53c8a20d68ae9346520bea0dffe6cf592b68e003
Agatha Sates <satesagatha@gmail.com>
2025-04-03 17:25:02 -0300

    Add project files.

---
+++ Gestao-de-Equipamentos.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestao_de_Equipamentos.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
