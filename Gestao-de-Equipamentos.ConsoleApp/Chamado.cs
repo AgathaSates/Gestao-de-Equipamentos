c73df6f3b862df1f8f54b2087dff85acc11af499
Agatha Sates <satesagatha@gmail.com>
2025-04-05 00:59:28 -0300

    feat: Adicionado objeto chamado

---
+++ Gestao-de-Equipamentos.ConsoleApp/Chamado.cs
@@ -0,0 +1,17 @@
+﻿namespace Gestao_de_Equipamentos.ConsoleApp;
+class Chamado
+{
+    public int iD;
+    public string titulo;
+    public string descricao;
+    public Equipamento equipamento;
+    public DateTime dataAbertura;
+
+    public Chamado(string titulo, string descricao, Equipamento equipamento)
+    {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.equipamento = equipamento;
+        this.dataAbertura = DateTime.Now;
+    }
+}
