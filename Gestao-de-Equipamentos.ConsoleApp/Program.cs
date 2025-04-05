bf54228ada9beca175f7699cc52350e0597bf9c2
Agatha Sates <satesagatha@gmail.com>
2025-04-05 02:31:09 -0300

    feat: Adicionado tela principal

--- Gestao-de-Equipamentos.ConsoleApp/Program.cs
+++ Gestao-de-Equipamentos.ConsoleApp/Program.cs
@@ -4,7 +4,75 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TelaEquipamento telaEquipamento = new TelaEquipamento();
+            TelaChamado telaChamado = new TelaChamado();
+
+            while (true)
+            {
+                TelaMenu.ApresentarMenu();
+                string opcaoescolhida = Console.ReadLine();
+                switch (opcaoescolhida)
+                {
+                    case "1": MenuEquipamentos(telaEquipamento); break;
+
+                    case "2": MenuChamados(telaChamado,telaEquipamento); break;
+                    default:
+                        Console.WriteLine("Saindo do programa...");
+                        return;
+
+                }
+            }
+        }
+
+        public static void MenuChamados(TelaChamado telaChamado, TelaEquipamento telaEquipamento)
+        {
+            string opcaoEscolhida = telaChamado.ApresentarMenu();
+            switch (opcaoEscolhida)
+            {
+                case "1":
+                    telaChamado.CadastrarChamado(telaEquipamento);
+                    break;
+                case "2":
+                    telaChamado.EditarChamado(telaEquipamento);
+                    break;
+                case "3":
+                    telaChamado.ExcluirChamado();
+                    break;
+                case "4":
+                    telaChamado.VisualizarChamados(true);
+                    break;
+                default:
+                    Console.WriteLine("Saindo do programa...");
+                    break;
+            }
+        }
+
+        public static void MenuEquipamentos(TelaEquipamento telaEquipamento)
+        {
+            string opcaoEscolhida = telaEquipamento.ApresentarMenu();
+
+            switch (opcaoEscolhida)
+            {
+                case "1":
+                    telaEquipamento.CadastrarEquipamento();
+                    break;
+
+                case "2":
+                    telaEquipamento.EditarEquipamento();
+                    break;
+
+                case "3":
+                    telaEquipamento.ExcluirEquipamento();
+                    break;
+
+                case "4":
+                    telaEquipamento.VisualizarEquipamentos(true);
+                    break;
+
+                default:
+                    Console.WriteLine("Saindo do programa...");
+                    break;
+            }
         }
     }
 }
